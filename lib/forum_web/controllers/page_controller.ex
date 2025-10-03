d8021c42dc93dbc5d247d8352e225cfa616d7640
Bruno Varela <brunohilariovarela@hotmail.com>
2025-10-03 17:22:44 +0200

    create API and databases

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,14 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  def users(conn, _params) do
+    IO.puts("Users function hit!")
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Maria", email: "maria@email.com"},
+    ]
+    #render(conn, :users, users: users)
+    json(conn, %{users: users})
+  end
 end
